8f48422c94b0f6cc77385c42f3ca89f35a716fae
jxsylar <jxsylar@outlook.com>
2019-07-07 17:13:08 +0800

    Update pyv8_test.py

--- pyv8_test.py
+++ pyv8_test.py
@@ -3,7 +3,18 @@
 
 import PyV8
 
-ctxt = PyV8.JSContext()
-ctxt.enter()
-func = ctxt.eval("""(function(){function hello(){return "Hello world.";}return hello();})""")
-print(func())
+with PyV8.JSLocker():
+    ctxt = PyV8.JSContext()
+
+def tes():
+    with PyV8.JSLocker():
+        ctxt.enter()
+        func = ctxt.eval("""(function(){function hello(){return "Hello world.";}return hello();})""")
+        print(func())
+        ctxt.leave()
+        PyV8.JSUnlocker()
+        PyV8.JSEngine.collect()
+
+if __name__ == '__main__':
+    tes()
+
